bb3eb958920fa2f3cdfda922a502f1a9a40ef4ae
Grant Cummings <grantcummings4429@gmail.com>
2024-05-14 09:05:12 -0400

    finished practice

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    if len(items) < 2:
+        return "".join(items)
+    last_element = items[-1]
+    add_and = "and " + last_element
+    items[-1] = add_and
+    if len(items) == 2:
+        return " ".join(items)
+    else : return ", ".join(items)
+    
+
+print(oxford_comma(["fiddleheads", "okra", "kohlrabi"]))
